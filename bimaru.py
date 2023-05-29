ad0e8c414f4eefffb80d3ea5a9cd63195861b794
joaoafonso07 <joaoafonsomestre@gmail.com>
2023-05-29 10:55:58 +0100

    v2.0 - Giant Commit

--- bimaru.py
+++ bimaru.py
@@ -3,8 +3,8 @@
 # Além das funções e classes já definidas, podem acrescentar outras que considerem pertinentes.
 
 # Grupo 00:
-# 00000 Nome1
-# 00000 Nome2
+# ist1102779 João Afonso Mestre
+# ist1 Miguel Benjamim 
 
 import sys
 from search import (
@@ -18,8 +18,12 @@ from search import (
 )
 
 board_size = 10
-boar_pieces = ('W', 'C', 'T', 'M', 'B', 'L', 'R')
+board_pieces = ('W', 'C', 'T', 'M', 'B', 'L', 'R')
 
+
+def valid_coord(row, col):
+    return row < board_size and row >= 0 and col < board_size and col >= 0
+         
 
 class BimaruState:
     state_id = 0
@@ -38,22 +42,23 @@ class BimaruState:
 class Board:
     """Representação interna de um tabuleiro de Bimaru."""
 
-    def __init__(self, row_restritions:tuple, column_restritions: tuple, grid, available_boats):
+    boats = {"boat1": 4, "boat2": 3, "boat3": 2, "boat4": 1}
+    
+    def __init__(self, row_restritions:tuple, column_restritions: tuple, grid):
         self.row_restritions = row_restritions
         self.column_restritions = column_restritions
         self.grid = grid
-        self.available_boats = available_boats
 
     def get_value(self, row: int, col: int) -> str:
         """Devolve o valor na respetiva posição do tabuleiro."""
-        if row > board_size or row < 0 or col > board_size or col < 0:
+        if not valid_coord(row, col):
             return -1
         return self.grid[row][col]
 
     def adjacent_vertical_values(self, row: int, col: int):
         """Devolve os valores imediatamente acima e abaixo,
         respectivamente."""
-        if row > board_size or row < 0 or col > board_size or col < 0:
+        if not valid_coord(row, col):
             return -1
         elif row == board_size:
             return (self.grid[row-1][col], None)
@@ -65,7 +70,7 @@ class Board:
     def adjacent_horizontal_values(self, row: int, col: int):
         """Devolve os valores imediatamente à esquerda e à direita,
         respectivamente."""
-        if row > board_size or row < 0 or col > board_size or col < 0:
+        if not valid_coord(row, col):
             return -1
         elif col == board_size:
             return (self.grid[row][col-1], None)
@@ -109,7 +114,48 @@ class Board:
         for row in self.grid:
             for i in row:
                 print(i)
-            print('\n')    
+            print('\n')
+
+    # 1 if cell (row, col) contains symbol piece, 0 otherwise (x), -1 in case of error (x)
+    def is_value(self, row, col, piece): 
+        if not valid_coord(row, col):
+            return -1
+        if self.get_value(row, col) == piece:
+            return 1
+        else:
+            return 0
+        
+    #denotes the number of boats that has to be placed in the grid for each sort (S)
+    def available_boats(self, boat): 
+        return self.boats[boat]
+    
+    #1 if a boats of length l starts (i.e., has symbol L, T, or C) in cell (row, col), 0 otherwise, -1 in case of error (s)
+    def boat_start(self, row, col, l):
+        if not valid_coord(row, col):
+            return -1
+        start_piece = self.grid[row][col]
+        if start_piece == 'L':
+            if not valid_coord(row, col + l - 1):
+                return -1
+            if self.grid[row][col + l - 1] == 'R':    #maybe check the midle of the boat
+                return 1
+            else:
+                return 0
+        elif start_piece == 'T':
+            if not valid_coord(row + l -1, col):
+                return -1
+            if self.grid[row + l - 1][col] == 'R':    #maybe check the midle of the boat
+                return 1
+            else:
+                return 0
+        elif start_piece == 'C':
+            if l == 1:
+                return 1
+            else:
+                return 0
+        else:
+            return 0
+    
 
 
     # TODO: outros metodos da classe
@@ -124,7 +170,19 @@ class Bimaru(Problem):
         """Retorna uma lista de ações que podem ser executadas a
         partir do estado passado como argumento."""
         # TODO
-        pass
+        
+        if state.state_id == 1: #if is the first state return 1
+            return 1
+        
+        actions = [] #(boatkind, row, col, start_piece)
+        board = state.board
+        available_boats = board.boats
+
+        if available_boats['boat4'] != 0:
+              for row in range(board_size):
+                for col in range(board_size):
+                    if board.row_restritions[row] >= 4:
+
 
     def result(self, state: BimaruState, action):
         """Retorna o estado resultante de executar a 'action' sobre
@@ -132,24 +190,130 @@ class Bimaru(Problem):
         das presentes na lista obtida pela execução de
         self.actions(state)."""
         # TODO
-        pass
+        board = state.board
+        row_parts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        col_parts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+        if action == 1:
+            for row in range(board_size):
+                for col in range(board_size):
+                    piece = board.grid[row][col]
+                    if piece == 'C':
+                        row_parts[row] += 1
+                        col_parts[col] += 1
+                        board.boats['boat1'] -= 1
+                        if row == 0:
+                            if col == 0:
+                                board.grid[row+1][col] = 'W'
+                                board.grid[row][col+1] = 'W'
+                                board.grid[row+1][col+1] = 'W'
+                            elif col == 9:
+                                board.grid[row+1][col] = 'W'
+                                board.grid[row][col-1] = 'W'
+                                board.grid[row+1][col-1] = 'W'
+                            else:
+                                board.grid[row][col-1] = 'W'
+                                board.grid[row][col+1] = 'W'
+                                board.grid[row+1][col] = 'W'
+                                board.grid[row+1][col-1] = 'W'
+                                board.grid[row+1][col+1] = 'W'
+                        elif row == 9:
+                            if col == 0:
+                                board.grid[row-1][col] = 'W'
+                                board.grid[row][col+1] = 'W'
+                                board.grid[row-1][col+1] = 'W'
+                            elif col == 9:
+                                board.grid[row-1][col] = 'W'
+                                board.grid[row][col-1] = 'W'
+                                board.grid[row-1][col-1] = 'W'
+                            else:
+                                board.grid[row][col-1] = 'W'
+                                board.grid[row][col+1] = 'W'
+                                board.grid[row-1][col] = 'W'
+                                board.grid[row-1][col-1] = 'W'
+                                board.grid[row-1][col+1] = 'W'
+                        else:
+                            if col == 0:
+                                board.grid[row-1][col] = 'W'
+                                board.grid[row-1][col+1] = 'W'
+                                board.grid[row][col+1] = 'W'
+                                board.grid[row+1][col+1] = 'W'
+                                board.grid[row+1][col] = 'W'
+                            elif col == 9:
+                                board.grid[row-1][col] = 'W'
+                                board.grid[row-1][col-1] = 'W'
+                                board.grid[row][col-1] = 'W'
+                                board.grid[row+1][col-1] = 'W'
+                                board.grid[row+1][col] = 'W'
+                            else:
+                                board.grid[row-1][col] = 'W'
+                                board.grid[row-1][col-1] = 'W'
+                                board.grid[row][col-1] = 'W'
+                                board.grid[row+1][col-1] = 'W'
+                                board.grid[row+1][col] = 'W'
+                                board.grid[row+1][col+1] = 'W'
+                                board.grid[row][col+1] = 'W'
+                                board.grid[row-1][col+1] = 'W'
+
+                    elif piece == 'T':
+                        row_parts[row] += 1
+                        col_parts[col] += 1
+                        board.grid[]
+
 
     def goal_test(self, state: BimaruState):
         """Retorna True se e só se o estado passado como argumento é
         um estado objetivo. Deve verificar se todas as posições do tabuleiro
         estão preenchidas de acordo com as regras do problema."""
         board = state.board
-        boat_pieces = [] #stores all the baot pices so we can then confirm if other goal restritions
-
+        
+        col_boat_parts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        count_boats = [0, 0, 0, 0] #[boat1, boat2, boat3, boat4]
         for row in range(board_size):
-            for col in range(board_size):
-                target = board.grid[row][col]
+            count_row_boat_parts = 0
+            for col in range(col_boat_parts):
+                piece = board.grid[row][col]
+                initial_piece = self.is_in_initial_grid(row, col)
 
-                if target == '.':
+                if piece not in board_pieces:    #1
                     return False
                 
-                if  target in boar_pieces and target != 'W':
-                    boat_pieces.append((target, row, col))                
+                if initial_piece in board_pieces:#2
+                    if piece == initial_piece:
+                        return False 
+                
+                if piece in board_pieces and piece != 'W':
+                    count_row_boat_parts += 1
+                    col_boat_parts[col] += 1
+
+                if piece == 'C': 
+                    if board.start_boat(row, col, 1) == 1: #5
+                        count_boats[0] += 1
+                    else:
+                        return False
+                
+                if piece != 'C': #5
+                    return board.start_boat(row, col, 1) == 0
+                
+                if piece == 'T' or piece == 'L': 
+                    if board.start_boat(row, col, 2) == 1: #6
+                        count_boats[1] += 1
+                    if board.start_boat(row, col, 3) == 1: #6
+                        count_boats[2] += 1
+                    if board.start_boat(row, col, 4) == 1: #6
+                        count_boats[3] += 1
+                    else:
+                        return False
+                
+            if count_row_boat_parts != board.row_restritions[row]: #3
+                return False
+            
+        if tuple(col_boat_parts) != board.col_restritions: #4
+            return False
+        if count_boats != [4, 3, 2, 1]: #7
+            return False
+
+        
 
     def h(self, node: Node):
         """Função heuristica utilizada para a procura A*."""
@@ -157,6 +321,8 @@ class Bimaru(Problem):
         pass
 
     # TODO: outros metodos da classe
+    def initial_grid_piece(self, row, col):
+        return self.board.get_value(row, col)
 
 
 if __name__ == "__main__":
@@ -165,4 +331,5 @@ if __name__ == "__main__":
     # Usar uma técnica de procura para resolver a instância,
     # Retirar a solução a partir do nó resultante,
     # Imprimir para o standard output no formato indicado.
-    pass
+    board = Board.parse_instance()
+    board.print()
